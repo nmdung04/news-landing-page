be7a0aec9e456d6e2eeb8580b19a53c9b684e2d6
nmdung04 <nmdung04@gmail.com>
2025-11-11 08:48:44 +0700

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,32 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              img-src 'self' https: data:;
-              media-src 'self' https:;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-              style-src 'self' 'unsafe-inline' https:;
-              frame-src https://www.youtube.com https://www.youtube-nocookie.com;
-            `.replace(/\n/g, "")
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           {
+//             key: "Content-Security-Policy",
+//             value: `
+//               default-src 'self';
+//               img-src 'self' https: data:;
+//               media-src 'self' https:;
+//               script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
+//               style-src 'self' 'unsafe-inline' https:;
+//               frame-src https://www.youtube.com https://www.youtube-nocookie.com;
+//             `.replace(/\n/g, "")
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
